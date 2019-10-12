312f62ad2211e07ea9b65e392e4d073279ab754b
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2019-10-12 18:50:44 -0400

    Add bot boilerplate

---
+++ bot.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	// Token the token for the bot
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + Token)
+
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		log.Printf(m.Content)
+	})
+
+	err = dg.Open()
+	if err != nil {
+		log.Println("error opening connections", err)
+		return
+	}
+
+	log.Println("Bot is now running. CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+
+}
