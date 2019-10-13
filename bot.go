603ce2248bbff4bfc3dde244c5663fce806e6156
CS-5 <me@carsonseese.com>
2019-10-13 14:26:15 +0000

    Setup logging and enviorment variable parsing

--- bot.go
+++ bot.go
@@ -1,47 +1,86 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	goenv "github.com/caarlos0/env/v6"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/sirupsen/logrus"
+)
+
+type (
+	enviorment struct {
+		Token string `env:"BOT_TOKEN"`
+		Debug bool   `env:"DEBUG" envDefault:"false"`
+	}
 )
 
 var (
-	// Token the token for the bot
-	Token string
+	env = enviorment{}
+	log *logrus.Logger
 )
 
 func init() {
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.Parse()
+	/* Parse enviorment variables */
+	if err := goenv.Parse(&env); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	fmt.Printf("%+v\n", env)
+
+	/* Define logging setup */
+	logrus.SetOutput(os.Stdout)
+	log = logrus.New()
+
+	logrus.SetLevel(logrus.DebugLevel)
+	log.SetFormatter(&logrus.TextFormatter{
+		ForceColors: true,
+	})
+
+	if !env.Debug {
+		logrus.SetLevel(logrus.InfoLevel)
+		log.SetFormatter(&logrus.JSONFormatter{})
+	}
 }
 
 func main() {
-	dg, err := discordgo.New("Bot " + Token)
+	log.Info("Starting Bot...")
+	dg, err := discordgo.New("Bot " + env.Token)
+	if err != nil {
+		log.WithField("error", err).Error("Problem starting bot")
+	}
+
+	log.Info("Bot started")
 
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
 
-		log.Printf(m.Content)
+		/* Log every message recieved */
+		ch, _ := s.Channel(m.ChannelID)
+		log.WithFields(logrus.Fields{
+			"author":  m.Author.Username,
+			"channel": ch.Name,
+			"message": m.Content,
+		}).Info("Message recieved")
 	})
 
 	err = dg.Open()
 	if err != nil {
-		log.Println("error opening connections", err)
+		log.WithField("error", err).Error(
+			"Problem opening websocket connection.",
+		)
 		return
 	}
+	defer dg.Close()
 
-	log.Println("Bot is now running. CTRL-C to exit.")
+	/* Wait for interrupt */
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
-	dg.Close()
-
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module 0x626f74
 
 go 1.13
 
-require github.com/bwmarrin/discordgo v0.19.0
+require (
+	github.com/bwmarrin/discordgo v0.19.0
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/caarlos0/env/v6 v6.1.0
+	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.4.2
+)
