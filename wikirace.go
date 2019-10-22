601fdb11f2e8acc7420ffd5fb31b696c4197548b
CS-5 <me@carsonseese.com>
2019-10-22 18:33:54 +0000

    Clean up code, better errors

--- wikirace.go
+++ wikirace.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+
+	"github.com/sirupsen/logrus"
 )
 
 type articleInfo struct {
@@ -12,29 +14,36 @@ type articleInfo struct {
 	Title string `json:"title"`
 }
 
-type queryResult struct {
-	Random []articleInfo `json:"random"`
-}
-
 type wikiResult struct {
-	BatchComplete string      `json:"batchComplete"`
-	Query         queryResult `json:"query"`
+	BatchComplete string `json:"batchComplete"`
+	Query         struct {
+		Random []articleInfo `json:"random"`
+	} `json:"query"`
 }
 
 func initWikiRace(ctx *context) {
 	m := ctx.Message
 	s := ctx.Session
 
+	/* TODO: Maybe float these erros up to the handler? */
 	resp, err := http.Get("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=2")
 	if err != nil {
-		log.Error(err)
+		log.WithFields(logrus.Fields{
+			"error":   err,
+			"command": ctx.Command,
+		}).Error("Unable to get random wikipedia page")
+
 		s.ChannelMessageSend(m.ChannelID, "Hmm... I seem to have run into an issue... Try again later?")
 		return
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Error(err)
+		log.WithFields(logrus.Fields{
+			"error":   err,
+			"command": ctx.Command,
+		}).Error("Unable to read page")
+
 		s.ChannelMessageSend(m.ChannelID, "Hmm... I seem to have run into an issue... Try again later?")
 		return
 	}
@@ -42,7 +51,11 @@ func initWikiRace(ctx *context) {
 	var search wikiResult
 	err = json.Unmarshal(body, &search)
 	if err != nil {
-		log.Error(err)
+		log.WithFields(logrus.Fields{
+			"error":   err,
+			"command": ctx.Command,
+		}).Error("Unable to unmarshal page")
+
 		s.ChannelMessageSend(m.ChannelID, "Hmm... I seem to have run into an issue... Try again later?")
 		return
 	}
@@ -50,6 +63,7 @@ func initWikiRace(ctx *context) {
 	articles := search.Query.Random[:2]
 
 	// @carson is there a better way of doing this?
+	/* TODO: Turn output into embedded message */
 	// <> characters prevent the embed
 	msg := "Race starts at " + articles[0].Title + " (<https://en.wikipedia.org/?curid=" + strconv.Itoa(articles[0].ID) + ">)" + " and goes to " + articles[1].Title + " (<https://en.wikipedia.org/?curid=" + strconv.Itoa(articles[1].ID) + ">)" + "."
 
